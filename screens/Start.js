9a6ee92e73b4982ffede8250298e05193b9e07f0
Baochuan Lu <lubaochuan@gmail.com>
2021-04-04 16:08:08 -0500

    animate start screen

--- screens/Start.js
+++ screens/Start.js
@@ -31,9 +31,59 @@ export default class Start extends React.Component {
   state = {
     transitionState: State.Launching,
   };
+  toggleOpacity = new Animated.Value(0);
+  buttonOpacity = new Animated.Value(0);
+
+  async componentDidMount() {
+    await sleep(500);
+
+    await configureTransition(() => {
+      this.setState({ transitionState: State.WillTransitionIn });
+    });
+
+    Animated.timing(this.toggleOpacity, {
+      toValue: 1,
+      duration: 500,
+      delay: 500,
+      useNativeDriver: true, }
+    ).start();
+
+    Animated.timing(this.buttonOpacity, {
+      toValue: 1,
+      duration: 500,
+      delay: 1000,
+      useNativeDriver: true, }
+    ).start();
+  }
 
   render() {
-    return null;
+    const { size, onChangeSize } = this.props;
+    const { transitionState } = this.state;
+
+    const toggleStyle = { opacity: this.toggleOpacity };
+    const buttonStyle = { opacity: this.buttonOpacity };
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <Logo />
+        </View>
+        {transitionState !== State.Launching && (
+          <Animated.View style={toggleStyle}>
+            <Toggle
+              options={BOARD_SIZES}
+              value={size}
+              onChange={onChangeSize}
+            />
+          </Animated.View>
+        )}
+        {transitionState !== State.Launching && (
+          <Animated.View style={buttonStyle}>
+            <Button title={'Start Game'} onPress={() => {}} />
+          </Animated.View>
+        )}
+      </View>
+    );
   }
 }
 
